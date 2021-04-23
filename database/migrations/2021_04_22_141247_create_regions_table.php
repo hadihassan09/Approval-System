15f54158a097bcf0441fc72e07e4910e18b6684d
Hadi Hassan <hadihassan09@gmail.com>
2021-04-23 08:40:52 +0300

    fixed table plural name

--- database/migrations/2021_04_22_141247_create_regions_table.php
+++ database/migrations/2021_04_22_141247_create_regions_table.php
@@ -13,7 +13,7 @@ class CreateRegionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('region', function (Blueprint $table) {
+        Schema::create('regions', function (Blueprint $table) {
             $table->id();
             $table->string('region');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateRegionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('region');
+        Schema::dropIfExists('regions');
     }
 }
